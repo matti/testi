41171bd1455e1bb0ee72d5638fc4c22f81f7bd28
Matti Luukkainen <mluukkai@iki.fi>
2016-09-20 12:57:41 +0300

    assets

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Railsdokkeri
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.relative_url_root = '/app'
+    #config.assets.prefix = '/app'
   end
 end
